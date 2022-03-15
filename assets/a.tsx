7385904acca3e162fddf76f3dc10b3ddfb1b417f
alexdevteam <alexpro820@gmail.com>
2022-03-15 15:04:06 +0100

    Update map

---
+++ assets/a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="a" tilewidth="18" tileheight="18" spacing="1" tilecount="81" columns="9">
+ <image source="pixel-platformer-blocks/Tilemap/marble.png" width="170" height="170"/>
+</tileset>
