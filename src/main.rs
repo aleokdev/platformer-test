2449d75c80a02a9a1b071c9460a43a2a09d078e6
alexdevteam <alexpro820@gmail.com>
2022-03-27 19:25:21 +0200

    Change window settings

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use ggez::*;
 use platformer_test::MainState;
 
 pub fn main() -> GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "aleok")
+    let cb = ggez::ContextBuilder::new("Platformer Template", "aleok")
         .window_setup(conf::WindowSetup::default().title("Platformer test"))
+        .window_mode(conf::WindowMode::default().dimensions(40. * 18., 25. * 18.))
         .add_resource_path(
             std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap_or_default())
                 .join("assets"),
