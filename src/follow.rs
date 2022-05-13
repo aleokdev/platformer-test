4f8315408d8d3a65d1e8840e6f078c212ac27676
aleokdev <aleok.inf@gmail.com>
2022-05-13 12:42:05 +0200

    Fix #1

--- src/follow.rs
+++ src/follow.rs
@@ -24,13 +24,13 @@ pub struct FollowPlugin;
 
 impl Plugin for FollowPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_to_stage(CoreStage::Last, follow);
+        app.add_system_to_stage(CoreStage::PostUpdate, follow);
     }
 }
 
 fn follow(
     mut query: Query<(&mut Transform, &OrthographicProjection, &CameraFollow)>,
-    target_q: Query<&GlobalTransform, Without<CameraFollow>>,
+    target_q: Query<&Transform, Without<CameraFollow>>,
 ) {
     for (mut transform, projection, follow) in query.iter_mut() {
         if let Some(target_transform) = follow.target.and_then(|t| target_q.get(t).ok()) {
